9951fcb26796d753d5f1ddf732624644edf57400
Mr.Viana <rafaelgov95@gmail.com>
2017-11-13 15:27:37 -0300

    up

--- SistemaParaFloricultura/android-wf/app/src/main/java/br/rv/wf/AppActivity.java
+++ SistemaParaFloricultura/android-wf/app/src/main/java/br/rv/wf/AppActivity.java
@@ -5,27 +5,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
 import com.mvc.imagepicker.ImagePicker;
-
-import org.json.JSONObject;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 
-import br.rv.wf.util.Imagens;
-import br.rv.wf.util.volley.FactorVolley;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -33,17 +22,22 @@ import butterknife.OnClick;
 public class AppActivity extends AppCompatActivity {
     @BindView(R.id.image_view)   ImageView imageView;
     @BindView(R.id.image_stream_indicator )  TextView textView;
+    @BindView(R.id.login_email) EditText login_email;
+    @BindView(R.id.login_password ) EditText login_password;
     Context contexto;
     Bitmap bitmap;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.activity_foto);
         contexto = this;
         ButterKnife.bind(this);
-        imageView = (ImageView) findViewById(R.id.image_view);
-        textView = (TextView) findViewById(R.id.image_stream_indicator);
-
+//        imageView = (ImageView) findViewById(R.id.image_view);
+//        textView = (TextView) findViewById(R.id.image_stream_indicator);
         ImagePicker.setMinQuality(600, 600);
+
+    }
+    @OnClick
+    public void  login(){
 
     }
 
